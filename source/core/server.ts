f0ebe321104a865e28aae952bf16c00840900cad
Victor101106 <victorgabriel101106+github@gmail.com>
2025-03-03 20:18:07 -0300

    refactor(server): export and add listeners

--- source/core/server.ts
+++ source/core/server.ts
@@ -2,6 +2,12 @@ import { instance } from './configs'
 
 const PORT = process.env.PORT
 
-instance.listen(PORT, () => {
+export const server = instance.listen(PORT)
+
+server.addListener("listening", () => {
     console.log(`⚡ Listening at PORT ${PORT}!`);
+})
+
+server.addListener("close", () => {
+    console.log(`🎈 Closing PORT ${PORT}!`);
 })
